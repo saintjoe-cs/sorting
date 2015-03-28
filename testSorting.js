bc0c8e3694b02ac7545323bd867a047d4294a11b
Brian Capouch <brianc@saintjoe.edu>
2015-03-28 03:21:56 -0400

    Add command-line arguments

--- testSorting.js
+++ testSorting.js
@@ -1,10 +1,26 @@
 load("sortingCode.js");
 
+var algorithm = arguments[0];
 var numElements = 100;
 var mynums = new CArray(numElements);
 mynums.setData();
+print('Executing: ' + algorithm);
 print(mynums.toString());
 print();
-mynums.shellSort2();
+switch(algorithm) {
+  case 'bubble':
+    mynums.bubbleSort();
+    break;
+  case 'shell': 
+    mynums.shellSort1();
+    break;
+  case 'insertion':
+    mynums.insertionSort();
+    break;
+  case 'selection':
+    mynums.selectionSort();
+    break;
+  }
 print();
 print(mynums.toString());
+print();
